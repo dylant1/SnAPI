890a3e06d74df6df9b5cefc3e67e32219ca82b6c
Dylan Toth <dylant1@yahoo.com>
2022-11-16 00:02:08 -0500

    Update card schema

--- models/Card.ts
+++ models/Card.ts
@@ -1,13 +1,19 @@
 import mongoose from "mongoose";
 
-const cardSchema = new mongoose.Schema({
-  name: String,
-  cost: Number,
-  power: Number,
-  //   ability: String,
-  abilityType: String,
-  description: String,
-  pool: Number,
-});
+const cardSchema = new mongoose.Schema(
+  {
+    name: String,
+    cost: Number,
+    power: Number,
+    description: String,
+    abilityType: String,
+    abilityID: Number,
+    pool: Number,
+    hasAbility: Boolean,
+  },
+  {
+    versionKey: false,
+  }
+);
 
 export default mongoose.model("Card", cardSchema);
